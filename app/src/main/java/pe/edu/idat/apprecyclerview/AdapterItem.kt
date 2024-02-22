293538319cb5ba72ad59da7c12e4b4715525c82d
edamago <daniel.martel@outlook.com>
2024-02-22 17:05:03 -0500

    se termino el adaptador

--- app/src/main/java/pe/edu/idat/apprecyclerview/AdapterItem.kt
+++ app/src/main/java/pe/edu/idat/apprecyclerview/AdapterItem.kt
@@ -1,4 +1,23 @@
 package pe.edu.idat.apprecyclerview
 
-class AdapterItem {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+
+import pe.edu.idat.apprecyclerview.databinding.ItemValorBinding
+
+class AdapterItem(private var lista:List<String>) : RecyclerView.Adapter<AdapterItem.ViewHolder>() {
+
+    inner class ViewHolder(val binding: ItemValorBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemValorBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return  ViewHolder(binding)
+    }
+
+    override fun getItemCount()=lista.size
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.tvItem.setText(lista.get(position))
+    }
 }
