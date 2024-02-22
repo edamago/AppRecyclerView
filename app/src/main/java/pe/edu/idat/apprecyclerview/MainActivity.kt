da1f31739e500a1400e027b58b58a04a0f8cea6d
edamago <daniel.martel@outlook.com>
2024-02-22 17:41:44 -0500

    se termino el ejercicio

--- app/src/main/java/pe/edu/idat/apprecyclerview/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/apprecyclerview/MainActivity.kt
@@ -2,10 +2,29 @@ package pe.edu.idat.apprecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import pe.edu.idat.apprecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvElementos.layoutManager=LinearLayoutManager(applicationContext)
+        binding.rvElementos.adapter=AdapterItem(listaElementos())
+    }
+
+    private fun listaElementos():List<String>{
+        val lista=ArrayList<String>()
+        var i=0
+        while(i<10)
+        {
+            lista.add("posicion $i")
+            i++
+        }
+        return lista
     }
 }
